87a41674a7b04006e69cf3a2bedd81960e306f51
mopilo <mopahemma@gmail.com>
2018-08-05 01:21:23 +0100

    runserver

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ const db = require('./config/keys').mongoURI;
 
 
 //connect to mongo db
-mongoose.connect(db)
+mongoose
+    .connect(db)
     .then(()=> console.log('Mongoose Connected ..........'))
     .catch(err => console.log(err))
+
+const port = process.env.PORT || 5000;
+app.listen(port, ()=> console.log(`Server started on port ${port} `));
