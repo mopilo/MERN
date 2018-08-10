4015b3c390aa8e6a6f8b82c0f55919ac75a532e9
mopilo <mopahemma@gmail.com>
2018-08-11 00:05:49 +0100

    addItem reducer

--- client/src/reducers/itemReducers.js
+++ client/src/reducers/itemReducers.js
@@ -1,12 +1,16 @@
 import uuid from 'uuid';
-import { GET_ITEMS } from '../actions/types'
+import { GET_ITEMS, DELETE_ITEMS, ADD_ITEMS } from '../actions/types'
 
 
 const initialState = {
     items: [
         { id: uuid(), name: 'Milk'},
         { id: uuid(), name: 'Eggs'},
-        { id: uuid(), name: 'Oil'}
+        { id: uuid(), name: 'Oil'},
+        { id: uuid(), name: 'Fish'},
+        { id: uuid(), name: 'Beans'}
+
+
     ] 
 }
 
@@ -16,6 +20,16 @@ export default function (state=initialState, action){
             return{
                 ...state
             } 
+        case DELETE_ITEMS:
+            return{
+                ...state,
+                items: state.items.filter(item=> item.id !== action.payload)
+            }
+            case ADD_ITEMS:
+            return{
+                ...state,
+                items: [action.payload, ...state.items]
+            }
         default:
             return state
     }
