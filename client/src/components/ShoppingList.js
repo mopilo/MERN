5f3a8bd03087ae76298160d95a5360d9660ebc7d
mopilo <mopahemma@gmail.com>
2018-08-11 00:05:16 +0100

    rename folder

---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react'
+import {Button, ListGroup, Container, ListGroupItem} from 'reactstrap'
+import {CSSTransition, TransitionGroup} from 'react-transition-group'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {getItems, deleteItems} from '../actions/itemActions'
+
+
+class ShoppingList extends Component{
+    
+
+    componentDidMount(){
+        this.props.getItems() 
+    }
+
+    itemDelete = (id) => {
+        this.props.deleteItems(id);
+    }
+
+    render(){
+        const {items} = this.props.item
+        return(
+            <Container>
+                
+                <ListGroup>
+                    <TransitionGroup className='shopping-list'>
+                        {items.map(({id, name})=> (
+                            <CSSTransition key={id} classNames='fade' timeout={500}>
+                                <ListGroupItem>
+                                    <Button
+                                        className='remove-btn'
+                                        color='danger'
+                                        size='sm'
+                                        onClick={this.itemDelete.bind(this, id)}
+                                    >
+                                        &times;
+                                    </Button>
+                                    {name}
+                                </ListGroupItem>
+                            </CSSTransition>
+                        )) }
+                    </TransitionGroup>
+                </ListGroup>
+            </Container>
+        )
+    }
+}
+
+ShoppingList.PropTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired,
+    deleteItems: PropTypes.func.isRequired
+
+}
+
+const mapStateToProps = (state) => ({
+    item: state.item
+})
+
+export default connect(mapStateToProps, {getItems, deleteItems})(ShoppingList)
