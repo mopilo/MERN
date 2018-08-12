6fe3704cb932697a87eb42b402ee1764635754dc
mopilo <mopahemma@gmail.com>
2018-08-12 08:16:09 +0100

    link to backend

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -1,21 +1,34 @@
-import {GET_ITEMS, DELETE_ITEMS, ADD_ITEMS} from './types'
-
-export const getItems = () => {
-    return{
-        type: GET_ITEMS
-    }
+import {GET_ITEMS, DELETE_ITEMS, ADD_ITEMS, ITEMS_LOADING} from './types'
+import axios from 'axios';
+export const getItems = () => dispatch=> {
+    dispatch(setItemLoading());
+    axios
+        .get('/api/item')
+        .then(res=>dispatch({
+            type: GET_ITEMS,
+            payload: res.data
+        }))
 }
 
-export const deleteItems = id => {
-    return{
+export const deleteItems = id => dispatch=> {
+    axios.delete(`/api/item/${id}`).then(res => dispatch({
         type: DELETE_ITEMS,
         payload: id
-    }
+    }))
+    
 }
 
-export const addItem = item => {
+export const addItem = item => dispatch=> {
+        axios
+            .post('/api/item', item)
+            .then(res => dispatch({
+                type: ADD_ITEMS,
+                payload: res.data
+            }))
+}
+
+export const setItemLoading = () => {
     return{
-        type: ADD_ITEMS,
-        payload: item
+        type: ITEMS_LOADING
     }
 }
