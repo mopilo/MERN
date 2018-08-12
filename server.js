a3ad19f4aa107dc4ee13ab0174ae61edaab80416
mopilo <mopahemma@gmail.com>
2018-08-12 08:48:12 +0100

    version 1.0.0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParse = require('body-parser');
+const path = require('path')
 
 const app = express();
 
@@ -24,6 +25,16 @@ mongoose
 
 // routes
 app.use("/api/item", item)
+
+
+//static assets
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    }
+)}
 
 
 const port = process.env.PORT || 5000;
